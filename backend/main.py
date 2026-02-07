d960ea4cb63783f6d6e46d59156df74d9bb5ee5e
Rachit288 <rachit2808@gmail.com>
2026-02-07 12:52:53 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI(title="Safe-Scan Lite API", version="1.0.0")
 # Enable CORS for frontend communication
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "https://safe-scan-lite-cqnf.vercel.app/"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "https://safe-scan-lite-cqnf.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
